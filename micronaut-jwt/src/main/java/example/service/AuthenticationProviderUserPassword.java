00df4cc832e77afc441ff58404e6f638484af170
Alex Rocha <alex.rochas@yahoo.com.br>
2018-08-14 10:49:32 -0300

    feat: add JWT example using micronaut

---
+++ micronaut-jwt/src/main/java/example/service/AuthenticationProviderUserPassword.java
@@ -0,0 +1,21 @@
+package example.service;
+
+import io.micronaut.security.authentication.*;
+import io.reactivex.Flowable;
+import org.reactivestreams.Publisher;
+
+import javax.inject.Singleton;
+import java.util.ArrayList;
+
+@Singleton
+public class AuthenticationProviderUserPassword implements AuthenticationProvider {
+
+    @Override
+    public Publisher<AuthenticationResponse> authenticate(AuthenticationRequest authenticationRequest) {
+        if ( authenticationRequest.getIdentity().equals("sherlock") &&
+                authenticationRequest.getSecret().equals("password") ) {
+            return Flowable.just(new UserDetails((String) authenticationRequest.getIdentity(), new ArrayList<>()));
+        }
+        return Flowable.just(new AuthenticationFailed());
+    }
+}
